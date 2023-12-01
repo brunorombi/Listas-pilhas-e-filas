4aa0771330e05c232b9fdd1499bd7fb246c0f68e
Bruno Rombi <brunorombi@yahoo.com>
2023-12-01 17:27:52 -0300

    implementando listas

---
+++ apps/test_linked_list.c
@@ -0,0 +1,16 @@
+#include "linked_list.h"
+
+
+int main() {
+
+    LinkedList *L = LinkedList_create();
+    
+    LinkedList_add_first(L, 5);
+    LinkedList_add_first(L, 4);
+    LinkedList_add_first(L, 2);
+    LinkedList_add_first(L, 10);
+
+    LinkedList_print(L);
+
+    return 0;
+}
---
+++ src/linked_list.c
@@ -0,0 +1,44 @@
+#include "linked_list.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _snode {
+    int val;
+    struct _snode *next;
+} SNode;
+
+
+typedef struct _linked_list {
+    SNode *begin;
+} LinkedList;
+
+LinkedList *LinkedList_create(){
+    LinkedList *L = (LinkedList *) calloc(1, sizeof(LinkedList));
+    L->begin = NULL;
+
+    return L;
+}
+
+SNode *SNode_create(int val){
+    SNode *p = (SNode*) calloc (1, sizeof(SNode));
+    p->val = val;
+    p->next = NULL;
+
+    return p;
+}
+
+void LinkedList_add_first(LinkedList *L, int val){
+    SNode *p = SNode_create(val);
+    p->next = L->begin;
+    L->begin = p;
+}
+
+void LinkedList_print(const LinkedList *L){
+    SNode *p = L->begin;
+    printf("L -> ");
+    while(p != NULL){
+        printf("%d -> ", p->val);
+        p = p->next;
+    }
+    printf("NULL");
+}
